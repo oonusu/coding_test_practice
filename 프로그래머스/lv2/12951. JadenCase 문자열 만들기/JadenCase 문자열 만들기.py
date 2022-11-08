9cf9cfce9ab7448d73e3d05d98cd818d97c41b78
oonusu <700ws@naver.com>
2022-11-08 10:14:14 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,15 +1,8 @@
 def solution(s):
-    s = list(s.lower())
-    for i, w in enumerate(s):
-        if i == 0:
-            if w.isalpha():
-                s[i] = w.upper()
-                continue
-                
-        if w == ' ':
-            
-            if i+1 < len(s) and s[i+1].isalpha():
-                s[i+1] = s[i+1].upper()
-            continue
-        
-    return ''.join(s)
+    words = list(s.split(' '))
+    answer = []
+
+    for w in words:
+        answer.append(w.capitalize())
+    
+    return ' '.join(answer)
